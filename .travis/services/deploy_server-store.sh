1cef1124adfb61ff447cdfd3b1c3c901f289e6ff
Germain <germain.gau@gmail.com>
2017-11-19 00:54:23 +0100

    WIP: added query module image push

--- .travis/services/deploy_server-store.sh
+++ .travis/services/deploy_server-store.sh
@@ -9,3 +9,8 @@ cd services/server-store/command
 container_name=server-store-command-amqp
 docker build -t $container_name .
 push_docker_image $container_name
+
+cd services/server-store/query
+container_name=server-store-query-http
+docker build -t $container_name .
+push_docker_image $container_name
